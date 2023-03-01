1a000c2fe4d762e7c098a7ca8a9e8db005a2f610
David Boddie <david.boddie@canonical.com>
2023-03-01 15:40:31 +0100

    Used a library (zstd) that isn't bundled with Python.

--- test-ctypes.py
+++ test-ctypes.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 import ctypes
-import os, sys
+import os, pprint, sys
 from ctypes.util import find_library
 
 if __name__ == '__main__':
 
-    path = find_library('lzma')
+    path = find_library('zstd')
     if not path:
-        sys.stderr.write('Failed to find liblzma.\n')
+        sys.stderr.write('Failed to find libzstd.\n')
         sys.exit(1)
 
     try:
         lib = ctypes.cdll.LoadLibrary(path)
     except OSError:
-        sys.stderr.write('Failed to load liblzma.\n')
+        sys.stderr.write('Failed to load libzstd.\n')
         sys.exit(1)
 
-    print(os.getenv("LD_LIBRARY_PATH"))
-    fn = lib.lzma_version_string
+    pprint.pprint(dict(os.environ))
+    fn = lib.ZSTD_versionString
     fn.restype = ctypes.c_char_p
-    print("liblzma version is " + str(fn(None), "ascii"))
+    print("libzstd version is " + str(fn(None), "ascii"))
