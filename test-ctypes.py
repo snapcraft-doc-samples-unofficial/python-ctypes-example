0ee06eed2032bc2a5afcbd1f5d0b4ffb3f2ef736
David Boddie <david.boddie@canonical.com>
2023-03-02 15:42:41 +0100

    Added ctypes patching script.

--- test-ctypes.py
+++ test-ctypes.py
@@ -6,6 +6,10 @@ from ctypes.util import find_library
 
 if __name__ == '__main__':
 
+    #if not os.getenv("RUNNING"):
+    #    os.system("RUNNING=1 strace " + __file__ + " 2>&1 | grep zstd")
+    #    exit()
+
     path = find_library('zstd')
     if not path:
         sys.stderr.write('Failed to find libzstd.\n')
